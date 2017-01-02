d0343ab36d1173cdfd7c017acaf7aab2af965cf2
Maximilian Fellner <max.fellner@gmail.com>
2017-01-02 23:57:52 +0100

    fix: package dependencies

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ from distutils.core import setup
 
 setup(
     name='upcwifioff',
-    version='0.1.1',
+    version='0.1.2',
     author='Maximilian Fellner',
     author_email='max.fellner@gmail.com',
     url='https://github.com/mfellner/upcwifioff',
     license='MIT',
     packages=['upcwifioff'],
+    install_requires=[
+        'selenium>=3.0.2,<4',
+        'splinter>=0.7.5,<1',
+        'tqdm>=4.10.0,<5'
+    ],
     entry_points={
       'console_scripts': [
-          'my_project = upcwifioff.__main__:main'
+          'upcwifioff = upcwifioff.__main__:main'
       ]
     }
 )
